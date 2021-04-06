2693a3cb0183fc87f4d6784fa390d01b503c263c
Yuri Martins <yuri.alessandro@hotmail.com>
2021-04-06 18:27:05 -0300

    read imported file

--- scripts/importer.js
+++ scripts/importer.js
@@ -3,7 +3,7 @@ Hooks.on("renderSidebarTab", async (app, html) => {
     let button = $("<button class='import-fichas-nimb'><i class='fas fa-file-import'></i> Importar Ficha de Nimb</button>")
 
     button.click(function () {
-      console.log('Botão clicado');
+      new FDNImporter().render(true);
     });
 
     html.find(".directory-footer").append(button);
@@ -11,5 +11,70 @@ Hooks.on("renderSidebarTab", async (app, html) => {
 });
 
 Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins it's initialization workflow.");
+  // game.settings.register("fichas-de-nimb-fvvt", "importSettings", {
+  //   name: "Local padrão do Fichas de Nimb",
+  //   scope: "client",
+  //   config: false,
+  //   default: {
+  //     source: "data",
+  //     extension: "png",
+  //     bucket: "",
+  //     region: "",
+  //     path: "worlds/" + game.world.name,
+  //     offset: 0.1,
+  //     fidelity: 3,
+  //     multiImageMode: "g",
+  //     webpConversion: true,
+  //     wallsAroundFiles: true,
+  //   }
+  // })
 });
+
+
+class FDNImporter extends Application {
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    options.id = "import-fichas-nimb";
+    options.template = "modules/fichas-de-nimb-fvvt/templates/importer.html"
+    options.classes.push("fdn-importer");
+    options.resizable = true;
+    options.height = "auto";
+    options.width = 400;
+    options.minimizable = true;
+    options.title = "Importador de Fichas de Nimb"
+    return options;
+  }
+
+  static async createActor(sheet) {
+    console.log(sheet);
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html)
+
+    html.find(".import-sheet").click(async ev => {
+      const sheetInput = html.find("[name=ficha]");
+      const sheetFile = sheetInput[0].files[0]
+
+      ui.notifications.notify("Enviando arquivo...");
+      const filename = `${sheetFile.name}${Math.floor(Math.random() * 1000000)}`;
+      FDNImporter.uploadFile(sheetFile, filename);
+
+      var reader = new FileReader();
+      reader.addEventListener('load', function() {
+        var result = JSON.parse(reader.result); // Parse the result into an object 
+        FDNImporter.createActor(result);
+      });
+
+      reader.readAsText(sheetFile);
+
+    });
+
+    this.close();
+  }
+
+  static async uploadFile(file, filename) {
+    let uploadFile = new File([file], filename + "." + 'json', { type: 'json' });
+    return await FilePicker.upload('data', '/t20actors/', uploadFile, {})
+  }
+}
